4157a618a42b9323aa2aa9a4b5a6496402879b4e
nativeTiger <santakumal41@gmail.com>
2023-04-25 02:20:59 +0545

    completed personal site

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-console.log("Hello world");
+import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// init Swiper:
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1.5,
+      spaceBetween: 40,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+});
+
+/**
+ * @description  When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+ */
+function scrollHeader() {
+  const header = document.getElementById("header");
+
+  if (this.scrollY >= 200) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+
+window.addEventListener("scroll", scrollHeader);
