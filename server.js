9874cd99d19d98c42299a1e39b8916de31bc0d2e
Kiran Kumal <kiran.kumal714@gmail.com>
2026-04-27 23:43:50 +0545

    feat: implement express-session for request tracking and update nginx load balancing to ip_hash

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { fileURLToPath } from "url";
 import path from "path";
+import session from "express-session";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,10 +10,27 @@ const app = express();
 const port = 3000;
 const application_name = process.env.APP_NAME;
 
+app.use(
+  session({
+    secret: "CbNlR_B1paqnhwA7VIcaaub0YZW13i-6O-0pIqtDfLvYq3Tn6MTNj9uutTDuBIiw",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 24 hours
+      httpOnly: true,
+      secure: false,
+    },
+  }),
+);
 app.use(express.static("dist"));
 
 app.get("/hello", (req, res) => {
-  console.log(`Hello From ${application_name}`);
+  if (!req.session.views) req.session.views = 0;
+  req.session.views++;
+  // console.log(`Hello From ${application_name}`);
+  console.log(
+    `[${application_name}] Views: ${req.session.views} | SessionID: ${req.sessionID}`,
+  );
   res.sendFile(__dirname + "/dist/index.html");
 });
 
